c891c5f23ef207d95a2c1b4a89168464d7d499f6
Caden <82663346+cwells611@users.noreply.github.com>
2025-01-16 23:44:37 -0700

    Update caden_model.py

--- caden_model.py
+++ caden_model.py
@@ -1,20 +1,3 @@
-# COMPETITION
-
-#########################################
-# DO NOT MODIFY THIS SECTION
-
-# load the competition data
-# The data is in the numpy array format:
-#   competition_images: (100,32,32,3) contains 100 images
-#   competition_labels: (100,1) contains class lables (0 to 9)
-import numpy as np
-competition_data = np.load('competition_data.npz') 
-competition_images = competition_data['competition_images']
-competition_labels = competition_data['competition_labels']
-
-
-#########################################
-# YOUR CODE/MODEL GOES HERE:
 import tensorflow as tf
 from tensorflow.keras import models, datasets
 from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, MaxPool2D, Dropout, Dense, Flatten
@@ -127,11 +110,4 @@ my_model = models.load_model('my_model.keras')
 #evaluate using cifar-10 test set 
 test_loss, test_accuracy = model.evaluate(test_images, test_labels, verbose=0)
 print(f'Test Accuracy: {test_accuracy*100:.2f}%')
-
-# evaluate your model on the competition data
-# make any adjustment to the data format as needed to run your model
-# you must return accuracy of your model on the competition data 
-competition_loss, competion_acc = my_model.evaluate(competition_images,  competition_labels)
-
-# MUST PRINT OUT THE ACCURACY OF YOUR MODEL ON THE COMPETITION DATA
-print('Accuracy:', competion_acc)
+print(f'Test Loss: {test_loss*100:.2f}%')
